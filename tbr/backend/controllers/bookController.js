d8258c8a9bbb49355c728757062610abd596756b
Emily Hoang <emilyhoanggs@gmail.com>
2025-11-27 16:46:24 -0500

    changes to db, completed get working on post for book controls

--- tbr/backend/controllers/bookController.js
+++ tbr/backend/controllers/bookController.js
@@ -1,10 +1,13 @@
-import pool from database.js;
+import pool from "database.js";
 
 // get functions
 
 export const getAllBooks = async (req, res) => {
-try     {
-  const result = await pool.query("SELECT * FROM book");
+  try {
+    const { user_id } = req.query;
+    const result = await pool.query(`SELECT * from books where user_id = $1`, [
+      user_id,
+    ]);
     res.json({ books: result.rows });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -12,23 +15,59 @@ try     {
 };
 
 export const getAllBooksTbr = async (req, res) => {
-    try {
-        const result = await pool.query("select * from book where in_library = false");
-        res.json({ books: result.rows });
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
-}
+  try {
+    const { user_id } = req.query;
+
+    const result = await pool.query(
+      `select * from books 
+        where in_library = false 
+        and user_id = $1`,
+      [user_id]
+    );
+    //the array ^^ $1 is getting information from
+    // ensures that people dont just put in sql
+
+    res.json({ books: result.rows });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const getAllBooksLibrary = async (req, res) => {
-    try {
-        const result = await pool.query("select * from book where in_library = true");
-        res.json({ books: result.rows });
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
-}
+  try {
+    const { user_id } = req.query;
+
+    const result = await pool.query(
+      `select * from books 
+        where in_library = true and user_id = $1`,
+      [user_id]
+    );
+
+    res.json({ books: result.rows });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 //post functions
 
-export const addNewBooktoTbr = async (req, res) 
+export const addNewBooktoTbr = async (req, res) => {
+  try {
+    const { isbn, author, title, pages, publisher, is_owned, user_id, images } =
+      req.body;
+
+    if (!isbn) {
+      console.log("book needs isbn");
+      return res.status(400).json({ error: "isbn required" });
+    }
+    await pool.query(
+      `insert into book (isbn, author, title, pages,publisher,is_owned, in_library, user_id, images) 
+      values($1, $2, $3, $4, $5, $6, false, $7, $8)`,
+      [isbn, author, title, pages, publisher, is_owned, user_id, images]
+    );
+
+    res.status(201).json({ message: "book added to tbr", title });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
